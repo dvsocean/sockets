0cce00b934f015577170921a440f255efc41f968
dvsocean <dvsocean@icloud.com>
2018-04-24 19:17:54 -0700

    added io data transfer

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function(){
 	console.log('CONNECTED TO SOCKET/EXPRESS SERVER!');
-})
+});
+
+socket.on('message', function(message){
+	console.log("Incoming..");
+	console.log(message.text);
+});
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
+io.on('connection', function(socket){
 	console.log("USER CONNECTED VIA SOCKET.IO");
+
+	socket.on('message', function(message){
+		console.log("data received: " + message.text);
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the app, have fun cause it will self destruct in 5 seconds'
+	});
 });
 
 http.listen(PORT, function(){
