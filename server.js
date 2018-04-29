718614fb2f4c390879303a7e99283f0292982ea8
= <dvsocean@icloud.com>
2018-04-29 12:02:45 -0700

    wrapped up the timestamp feature

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
+var moment = require('moment');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -8,15 +9,17 @@ app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
 	console.log("USER CONNECTED VIA SOCKET.IO");
-
+	
 	socket.on('message', function(message){
+		message.timestamp = moment().valueOf();
 		console.log("data received: " + message.text);
 		//broadcast emits to every browser but my own
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the app, have fun but it will soon self destruct'
+		text: 'Welcome to the app, have fun but it will soon self destruct',
+		timestamp: moment().valueOf()
 	});
 });
 
